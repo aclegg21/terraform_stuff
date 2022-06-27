5dd6f4434c8d2c3fe73ceadd3f4a41b285f5d700
aclegg21 <aaron.clgg@gmail.com>
2022-06-27 13:11:37 -0600

    Update main.tf

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -14,39 +14,6 @@ resource "aws_ecs_cluster_capacity_providers" "ecs_cluster_providers" {
   }
 }
 
-resource "aws_ecr_repository_policy" "foopolicy" {
-  repository = aws_ecr_repository.foo.name
-
-  policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-        {
-            "Sid": "new policy",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:PutImage",
-                "ecr:InitiateLayerUpload",
-                "ecr:UploadLayerPart",
-                "ecr:CompleteLayerUpload",
-                "ecr:DescribeRepositories",
-                "ecr:GetRepositoryPolicy",
-                "ecr:ListImages",
-                "ecr:DeleteRepository",
-                "ecr:BatchDeleteImage",
-                "ecr:SetRepositoryPolicy",
-                "ecr:DeleteRepositoryPolicy"
-            ]
-        }
-    ]
-}
-EOF
-}
-
 module "ecs-fargate" {
   source  = "umotif-public/ecs-fargate/aws"
   version = "~> 6.1.0"

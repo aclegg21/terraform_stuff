092e9ee4a03a2276b449db28cbeedab07f71c4a1
Ubuntu <ubuntu@ip-172-31-89-152.ec2.internal>
2022-07-03 19:47:22 +0000

    More updates

--- terraform-docker/variables.tf
+++ terraform-docker/variables.tf
@@ -11,7 +11,7 @@ variable "image" {
       prod = "quay.io/influxdb/influxdb:v2.0.2"
     }
     grafana = {
-      dev = "grafana/grafana"
+      dev  = "grafana/grafana"
       prod = "grafana/grafana"
     }
   }
